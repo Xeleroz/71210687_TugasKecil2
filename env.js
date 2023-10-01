d3e275e473ad11038a0682fb68c2bede10428007
Mikael Herry Kristianto <92173554+Xeleroz@users.noreply.github.com>
2023-10-01 23:19:38 +0700

    tugas kecil 2

---
+++ env.js
@@ -0,0 +1,15 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+    API_KEY:
+        "",
+    endpoint: "https://api.thecatapi.com/",
+};
+
+const dog_env = {
+    API_KEY:
+        "live_tuORkhXyTBLRLtxyoHTVeUqSIxjEQzaJtWqPUi1MUTAZ17Hs84JeKiCt7Ftj7xVb",
+    endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { dog_env, cat_env };
